c4ea8fd0fff47ef1475845d82dd935cfb0acc82e
Usuario <Usuario@CPD02-M01>
2024-02-27 10:51:01 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto2702Adicao/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto2702Adicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definição das variáveis e entrada de dados
+            Console.WriteLine("Digite o primeiro número: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int resultado = n1 + n2;
+
+            Console.WriteLine("O resultado da soma é: " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
